49593000d33d4ac79015ee447431683b9c1b62c1
buketh <buket.hepgunay@gmail.com>
2022-07-17 13:44:20 +0300

    created weather app in react

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./App.css";
+import "./App";
 
 export default function Weather(props) {
   let [weather, setWeather] = useState(null);
